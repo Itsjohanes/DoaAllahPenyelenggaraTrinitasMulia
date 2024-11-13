d3620b260ce1c943aa61f523c6284ed54b7c85af
johannes <johannesap@upi.edu>
2024-11-13 21:56:17 +0700

    first commit

---
+++ app/src/main/java/com/example/ypiibandung/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.ypiibandung;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.widget.Button;
+public class MainActivity extends AppCompatActivity {
+    Button btnHymne, btnDoa;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        btnHymne = findViewById(R.id.btn_hymne);
+        btnDoa = findViewById(R.id.btn_doa);
+        btnHymne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Perintah Intent Explicit pindah halaman ke activity_detail
+                startActivity(new Intent(MainActivity.this, HymneTrinitas.class));
+            }
+        });
+
+        btnDoa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Perintah Intent Explicit pindah halaman ke activity_detail
+                startActivity(new Intent(MainActivity.this, Doa.class));
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
